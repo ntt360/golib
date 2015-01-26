8c91333605bf1c5ca13a6dc4d4f2835c2db8e9bd
ligang <ligang@invalid.com>
2015-01-26 10:31:34 +0800

    dao

--- dao/mysql/mysql_test.go
+++ dao/mysql/mysql_test.go
@@ -16,16 +16,18 @@ import (
 
 func init() {
 	bizlog.Init("/home/ligang/devspace/golib/logs")
-	bizlog.NewLogger(LOG_KEY_MYSQL, bizlog.MODE_SYNC, "", bizlog.SPLIT_BY_DAY, 0, bizlog.MSG_FMT_LINE_HEADER)
+	bizlog.NewLogger("test", bizlog.MODE_SYNC, "", bizlog.SPLIT_BY_DAY, 0, bizlog.MSG_FMT_LINE_HEADER)
 }
 
 func TestUpdateById(t *testing.T) {
-	key = "test"
-	mysql_conf := T_Mysql_Conf{
+	mysql_conf := executor.T_Mysql_Conf{
 		Host: "127.0.0.1",
 		User: "root",
 		Pass: "123",
 		Port: "3306",
 		Name: "test",
 	}
+
+	dao := NewBaseDao("test", mysql_conf, bizlog.GetLogger("test"))
+	dao.UpdateById("test", 8, []string{"id"}, 8)
 }
